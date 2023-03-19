02e4d6978ce7f684b6d5c0694b2f59382fe7b1b1
eran <eranavru100@gmail.com>
2023-03-19 17:24:06 +0200

    app.get try

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv"
 import { connectToDB } from "./connection";
 import routes from "./routes/index";
+import path from "path";
 
 dotenv.config()
 const app = express();
@@ -12,6 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(routes);
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
 
 const port = process.env.PORT
 app.listen(port, () => console.log("Listening on port"));

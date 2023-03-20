8e65514a7ee96cd7606868abcba5d2ee64965edf
eran <eranavru100@gmail.com>
2023-03-20 23:01:05 +0200

    Update users.routes.ts

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -25,7 +25,7 @@ userRouter.get("/get/all",authCheck(['admin']), getAllUsers);
 
 userRouter.get("/getCart", getUserCart);
 
-userRouter.delete("/delete/cartDish",UserCheck, deleteDishFromCart);
+userRouter.delete("/delete/cartDish", deleteDishFromCart);
 
 
 
